4e989c3cc4325df0f2a81a61bb1dcccf9673ba51
Xursandbek <xursandbek06082001@mail.com>
2023-11-02 22:31:42 +0500

    Initial Commit

---
+++ Animated_SwiftUI/Animated_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animated_SwiftUIApp.swift
+//  Animated_SwiftUI
+//
+//  Created by Xursandbek Qambaraliyev on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Animated_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
